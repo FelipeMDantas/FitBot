c3bbf053f6dd8142d1c4fec6158319778f689e2f
FelipeMDantas <felipemdantas@terra.com.br>
2025-05-17 16:56:28 -0300

    [feat] Build the Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
 import Navbar from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
