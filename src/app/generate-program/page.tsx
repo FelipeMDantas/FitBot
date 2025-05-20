e28aa077a8e3a1548450beb64221070254c7cb22
FelipeMDantas <felipemdantas@terra.com.br>
2025-05-19 22:04:39 -0300

    [feat] Code vapi event listeners

--- src/app/generate-program/page.tsx
+++ src/app/generate-program/page.tsx
@@ -1,6 +1,7 @@
+import { vapi } from "@/lib/vapi";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const GenerateProgramPage = () => {
   const [callActive, setCallActive] = useState(false);
@@ -13,6 +14,66 @@ const GenerateProgramPage = () => {
   const router = useRouter();
 
   const messageContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTop =
+        messageContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    const handleCallStart = () => {
+      console.log("Call started");
+      setConnecting(false);
+      setCallActive(true);
+      setCallEnded(false);
+    };
+
+    const handleCallEnd = () => {
+      console.log("Call ended");
+      setCallActive(false);
+      setConnecting(false);
+      setIsSpeaking(false);
+      setCallEnded(true);
+    };
+
+    const handleSpeechStart = () => {
+      console.log("AI started Speaking");
+      setIsSpeaking(true);
+    };
+
+    const handleSpeechEnd = () => {
+      console.log("AI stopped Speaking");
+      setIsSpeaking(false);
+    };
+    const handleMessage = (message: any) => {};
+
+    const handleError = (error: any) => {
+      console.log("Vapi Error", error);
+      setConnecting(false);
+      setCallActive(false);
+    };
+
+    vapi
+      .on("call-start", handleCallStart)
+      .on("call-end", handleCallEnd)
+      .on("speech-start", handleSpeechStart)
+      .on("speech-end", handleSpeechEnd)
+      .on("message", handleMessage)
+      .on("error", handleError);
+
+    return () => {
+      vapi
+        .off("call-start", handleCallStart)
+        .off("call-end", handleCallEnd)
+        .off("speech-start", handleSpeechStart)
+        .off("speech-end", handleSpeechEnd)
+        .off("message", handleMessage)
+        .off("error", handleError);
+    };
+  }, []);
+
   return <div>GenerateProgramPage</div>;
 };
 
